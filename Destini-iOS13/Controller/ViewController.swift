29571a1922f7e42a347e01e914d76812f525a389
Simon Oh <simon.oh@hey.com>
2023-09-12 21:56:54 -0500

    Initial commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
